440a3b70ca1379c522d011a0e0b63f04fca36ccf
Igor Andrade Silva <igor.andrade1013@gmail.com>
2021-09-07 11:36:22 -0300

    Agregando com objetos de valor

---
+++ src/NerdStore.Catalogo.Domain/Dimensoes.cs
@@ -0,0 +1,35 @@
+﻿using NerdStore.Core.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NerdStore.Catalogo.Domain
+{
+    public class Dimensoes
+    {
+        public decimal Altura { get; private set; }
+        public decimal Largura { get; private set; }
+        public decimal Profundidade { get; private set; }
+
+        public Dimensoes(decimal altura, decimal largura, decimal profundidade)
+        {
+            Validacoes.ValidarSeMenorIgualMinimo(altura, 1, "O campo altura não pode ser menor que ou igual a 0");
+            Validacoes.ValidarSeMenorIgualMinimo(largura, 1, "O campo largura não pode ser menor que ou igual a 0");
+            Validacoes.ValidarSeMenorIgualMinimo(profundidade, 1, "O campo profundidade não pode ser menor que ou igual a 0");
+
+            Altura = altura;
+            Largura = largura;
+            Profundidade = profundidade;
+        }
+
+        public string DescricaoFormatada()
+        {
+            return $"LxAxP: {Largura} x {Altura} x {Profundidade}";
+        }
+
+        public override string ToString()
+        {
+            return DescricaoFormatada();
+        }
+    }
+}
