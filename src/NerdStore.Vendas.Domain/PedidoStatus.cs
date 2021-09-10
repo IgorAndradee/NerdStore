17eddafa28cd33b92cfb491a5651b882ae62792a
Igor Andrade Silva <igor.andrade1013@gmail.com>
2021-09-10 19:25:21 -0300

    Objetos de domínio

--- src/NerdStore.Vendas.Domain/PedidoStatus.cs
+++ src/NerdStore.Vendas.Domain/PedidoStatus.cs
@@ -4,7 +4,12 @@ using System.Text;
 
 namespace NerdStore.Vendas.Domain
 {
-    class PedidoStatus
+    public enum PedidoStatus
     {
+        Rascunho = 0,
+        Iniciado = 1,
+        Pago = 4,
+        Entregue = 5,
+        Cancelado = 6
     }
 }
